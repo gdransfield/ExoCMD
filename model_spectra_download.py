48fe66df5cf7add2da652cba1a9d4050e0877b52
Georgina Garfield (MSc by Research Phys and Astro) <gxg831@eps-fvfyj2q.home>
2020-03-08 14:00:58 +0000

    updated download script to print progress

--- model_spectra_download.py
+++ model_spectra_download.py
@@ -12,24 +12,21 @@ import os
 import requests as req
 import numpy as np
 
-
 Model = ['{0:05d}'.format(x) for x in np.arange(1,10641)]
 urls = ['http://cdsarc.u-strasbg.fr/viz-bin/nph-Plot/w/Vgraph/txt?J%2fApJ%2f813%2f47%2f.%2f' \
             + i + '&--bitmap-size&600x400' for i in Model]
 
-
 loc = os.getcwd()
-filepaths = [os.path.join(loc, 'model_spectra', (i + '.txt.')) for i in Model]
-
+filepaths = [os.path.join(loc, 'model_spectra', (i + '.txt')) for i in Model]
 
 def downloads(url_list):
-    g = 0
-    while g < len(Model):
-        visit_page = req.get(url_list[g])
-        file = open(filepaths[g], 'w')
-        file.write(visit_page.text)
-        file.close()
-        g += 1
+    for filepath, url in zip(filepaths, url_list):
+        if not os.path.exists(filepath):
+            print(filepath)
+            visit_page = req.get(url)
+            with open(filepath, 'w') as open_file:
+                open_file.write(visit_page.text)
+
     return
 
-downloads (urls)
+downloads(urls)
